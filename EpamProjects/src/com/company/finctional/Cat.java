61204a67278527e63d1e0f54ea81dc482ec32fc4
MariiaKalugina <masha_k_o@mail.ru>
2018-10-28 14:58:18 +0300

    Add task with functional interface (refactoring).

--- EpamProjects/src/com/company/finctional/Cat.java
+++ EpamProjects/src/com/company/finctional/Cat.java
@@ -1,6 +1,6 @@
 package com.company.finctional;
 
-public class Cat implements WithNameAndAge{
+public class Cat implements WithNameAndAge {
     private String name;
     private int age;
     private String color;
--- EpamProjects/src/com/company/finctional/WithNameAndAge.java
+++ EpamProjects/src/com/company/finctional/WithNameAndAge.java
@@ -2,6 +2,8 @@ package com.company.finctional;
 
 public interface WithNameAndAge {
     void setName(String name);
+
     void setAge(int age);
+
     void setColor(String color);
 }
